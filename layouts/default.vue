7e3c1904a90f12fdc76644143fcdaaca0e33662f
Володимир <onmaine@gmail.com>
2020-08-04 18:28:26 +0300

    slider 0/3

--- layouts/default.vue
+++ layouts/default.vue
@@ -31,3 +31,52 @@ export default {
   }
 };
 </script>
+
+<style>
+/* полоса прокрутки (скроллбар) */
+::-webkit-scrollbar {
+  @apply w-2 h-2 bg-gray-400;
+}
+
+/* ползунок скроллбара */
+::-webkit-scrollbar-thumb {
+  @apply bg-teal-500 rounded-full shadow-inner;
+}
+
+::-webkit-scrollbar-thumb:hover {
+  @apply bg-red-400;
+}
+
+/* Стрелки */
+
+::-webkit-scrollbar-button:vertical:start:decrement {
+    background: linear-gradient(120deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
+                linear-gradient(240deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
+                linear-gradient(0deg, #fff5f7 30%, rgba(0, 0, 0, 0) 31%);
+    @apply bg-teal-500;
+}
+
+::-webkit-scrollbar-button:vertical:end:increment {
+    background:
+        linear-gradient(300deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
+        linear-gradient(60deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
+        linear-gradient(180deg, #fff5f7 30%, rgba(0, 0, 0, 0) 31%);
+    @apply bg-teal-500;
+}
+
+::-webkit-scrollbar-button:horizontal:start:decrement {
+    background:
+        linear-gradient(30deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
+        linear-gradient(150deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
+        linear-gradient(270deg, #fff5f7 30%, rgba(0, 0, 0, 0) 31%);
+    @apply bg-teal-500;
+}
+
+::-webkit-scrollbar-button:horizontal:end:increment {
+    background:
+        linear-gradient(210deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
+        linear-gradient(330deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
+        linear-gradient(90deg, #fff5f7 30%, rgba(0, 0, 0, 0) 31%);
+    @apply bg-teal-500;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -6,6 +6,7 @@
     <Services/>
     <Mobile/>
     <Presentation/>
+    <slider/>
 
   </div>
 </template>
@@ -17,6 +18,7 @@ import Stats from '@/components/Stats'
 import Services from '@/components/Services'
 import Mobile from '@/components/Mobile'
 import Presentation from '@/components/Presentation'
+import Slider from '@/components/Slider'
 
 export default {
   components: {
@@ -25,59 +27,12 @@ export default {
     Stats,
     Services,
     Mobile,
-    Presentation
+    Presentation,
+    Slider
   }
 };
 </script>
 
 <style>
-
-
-/* полоса прокрутки (скроллбар) */
-::-webkit-scrollbar {
-  @apply w-2 h-2 bg-gray-400;
-}
-
-/* ползунок скроллбара */
-::-webkit-scrollbar-thumb {
-  @apply bg-teal-500 rounded-full shadow-inner;
-}
-
-::-webkit-scrollbar-thumb:hover {
-  @apply bg-red-400;
-}
-
-/* Стрелки */
-
-::-webkit-scrollbar-button:vertical:start:decrement {
-    background: linear-gradient(120deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
-                linear-gradient(240deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
-                linear-gradient(0deg, #fff5f7 30%, rgba(0, 0, 0, 0) 31%);
-    @apply bg-teal-500;
-}
-
-::-webkit-scrollbar-button:vertical:end:increment {
-    background:
-        linear-gradient(300deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
-        linear-gradient(60deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
-        linear-gradient(180deg, #fff5f7 30%, rgba(0, 0, 0, 0) 31%);
-    @apply bg-teal-500;
-}
-
-::-webkit-scrollbar-button:horizontal:start:decrement {
-    background:
-        linear-gradient(30deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
-        linear-gradient(150deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
-        linear-gradient(270deg, #fff5f7 30%, rgba(0, 0, 0, 0) 31%);
-    @apply bg-teal-500;
-}
-
-::-webkit-scrollbar-button:horizontal:end:increment {
-    background:
-        linear-gradient(210deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
-        linear-gradient(330deg, #fff5f7 40%, rgba(0, 0, 0, 0) 41%),
-        linear-gradient(90deg, #fff5f7 30%, rgba(0, 0, 0, 0) 31%);
-    @apply bg-teal-500;
-}
 
 </style>
