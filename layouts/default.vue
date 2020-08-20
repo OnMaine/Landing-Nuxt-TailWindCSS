988e1f737232b995d5e4b0e829d8b3499442ffee
Володимир <onmaine@gmail.com>
2020-08-20 15:12:59 +0300

    config

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,26 +13,6 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 
 export default {
-  head: {
-    title: "Nuxt.js with TailwindCSS",
-    meta: [{
-        charset: "utf-8"
-      },
-      {
-        name: "viewport",
-        content: "width=device-width, initial-scale=1"
-      },
-      {
-        hid: "description",
-        name: "description",
-        content: "Nuxt.js with TailwindCSS example"
-      }
-    ],
-    link: [{
-      rel: "stylesheet",
-      href:"https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Montserrat:wght@400;700&family=Roboto:wght@400;700&display=swap"
-    }]
-  },
   components: {
     Header,
     Footer
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,21 @@
+
 export default {
-  /*
-   ** Nuxt.js modules
-   */
+  head: {
+    title: 'Nuxt.js with TailwindCSS',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: "stylesheet",
+        href:"https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Montserrat:wght@400;700&family=Roboto:wght@400;700&display=swap"
+      }
+    ]
+  },
+
   buildModules: [
     // Doc: https://github.com/nuxt-community/nuxt-tailwindcss
     "@nuxtjs/tailwindcss"
